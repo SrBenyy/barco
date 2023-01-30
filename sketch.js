1d8fe2a1a9e5a82f34cf13da0d2806407f2ef497
SrBenyy <122478595+SrBenyy@users.noreply.github.com>
2023-01-30 15:25:33 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+ 
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+  seaimg = loaudImage("sea.png");
+  shipImg1 = loaudImage("ship-1.png");
+
+
+
+}
